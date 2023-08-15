8525a2645543343731a31fe03740343f29eb9734
Zach Witzig <zachwitzig@Zachs-MacBook-Air.local>
2023-08-14 21:15:08 -0400

    add ability to check todo items

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { TodoItem } from "./components/TodoItem";
 
 export type TodoItemType = {
@@ -10,41 +11,49 @@ type TodoList = {
   todos: TodoItemType[];
 }
 
-function todoItemDataToTodoItemComponent(todoItemData: TodoItemType) {
-  return <TodoItem item={todoItemData} />
-}
+const DEFAULT_TODO_LIST: TodoList = {
+  title: "Todoist",
+  todos: [
+    {
+      name: "Make the todo object",
+      isDone: true
+    },
+    {
+      name: "Walk the dog",
+      isDone: false
+    },
+    {
+      name: "Feed goldfish",
+      isDone: false
+    },
+    {
+      name: "Eat goldfish",
+      isDone: false,
+    }
+  ],
+};
 
 function App() {
-  const myTodoList: TodoList = {
-    title: "Todoist",
-    todos: [
-      {
-        name: "Make the todo object",
-        isDone: true
-      },
-      {
-        name: "Walk the dog",
-        isDone: false
-      },
-      {
-        name: "Feed goldfish",
-        isDone: false
-      },
-      {
-        name: "Eat goldfish",
-        isDone: false,
-      }
-    ],
-  };
+  const [todoListItems, setTodoListItems] = useState<TodoItemType[]>(DEFAULT_TODO_LIST.todos);
+
+  const handleTodoItemChecked = (index: number, newCheckedState: boolean) => {
+    const todoListItem = todoListItems[index];
+
+    todoListItem.isDone = newCheckedState;
+
+    todoListItems[index] = todoListItem;
+
+    setTodoListItems([...todoListItems]);
+  }
 
   return (
     <div>
       <header>
-        <h1>{myTodoList.title}</h1>
+        <h1>{DEFAULT_TODO_LIST.title}</h1>
       </header>
       <section>
         {
-          myTodoList.todos.map(todoItemDataToTodoItemComponent)
+          todoListItems.map((todoListItem, index) => <TodoItem key={index} index={index} item={todoListItem} handleTodoItemChecked={handleTodoItemChecked} />)
         }
       </section>
     </div>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,16 +1,32 @@
+import { ChangeEvent } from "react";
 import { TodoItemType } from "../App";
 
 type TodoItemProps = {
+    index: number;
     item: TodoItemType;
+    handleTodoItemChecked: (index: number, newCheckedState: boolean) => void;
 }
 
-export function TodoItem({ item }: TodoItemProps) {
+export function TodoItem({ index, item, handleTodoItemChecked }: TodoItemProps) {
     const { isDone, name } = item;
 
+    const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const newCheckedState = event.target.checked;
+
+      handleTodoItemChecked(index, newCheckedState);
+    };
+
+    const handleDeleteClick = () => {
+      // TODO: build me!
+    }
+
     return (
-      <div>
-        <input type="checkbox" checked={isDone}/>
+      <div style={{
+        marginBottom: `2px`
+      }}>
+        <input type="checkbox" checked={isDone} onChange={handleOnChange}/>
         <p>{name}</p>
+        <button onClick={handleDeleteClick}>Delete</button>
       </div>
     );
   }
