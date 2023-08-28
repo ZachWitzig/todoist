31fc7c77847ca35eb4f0743fe3384d7aff271eaf
JP <jgpeters717@gmail.com>
2023-08-28 19:18:10 -0400

    reformat todoitem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,40 +2,50 @@ import { ChangeEvent } from "react";
 import { TodoItemType } from "../App";
 
 type TodoItemProps = {
-    index: number;
-    item: TodoItemType;
-    onTodoItemChecked: (index: number, newCheckedState: boolean) => void;
-    onDeleteClick: (index: number) => void;
-} 
+  index: number;
+  item: TodoItemType;
+  onTodoItemChecked: (index: number, newCheckedState: boolean) => void;
+  onDeleteClick: (index: number) => void;
+};
 
-export function TodoItem({ index, item, onTodoItemChecked, onDeleteClick }: TodoItemProps) {
-    const { isDone, name } = item;
+export function TodoItem({
+  index,
+  item,
+  onTodoItemChecked,
+  onDeleteClick,
+}: TodoItemProps) {
+  const { isDone, name } = item;
 
-    const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-      const newCheckedState = event.target.checked;
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newCheckedState = event.target.checked;
 
-      onTodoItemChecked(index, newCheckedState);
-    };
+    onTodoItemChecked(index, newCheckedState);
+  };
 
-    function handleDeleteClick() {
-      onDeleteClick(index);
-    }   
+  function handleDeleteClick() {
+    onDeleteClick(index);
+  }
 
-    function TodoItemName({name, isDone}: { name: string; isDone: boolean; }) {
-      if (!isDone) {
-          return (<p>{name}</p>);
-      }
-      
-      return (<p><s>{name}</s></p>);
+  function TodoItemName({ name, isDone }: { name: string; isDone: boolean }) {
+    if (!isDone) {
+      return <p>{name}</p>;
     }
 
-      return (
-        <div style={{
-          marginBottom: `2px`
-        }}>
-          <input type="checkbox" checked={isDone} onChange={handleOnChange}/>
-          <TodoItemName name={name} isDone={isDone} />
-          <button onClick={handleDeleteClick}>Delete</button>
-        </div>
-      );
-    }
+    return (
+      <p>
+        <s>{name}</s>
+      </p>
+    );
+  }
+
+  return (
+    <div
+      style={{
+        marginBottom: `2px`,
+      }}>
+      <input type="checkbox" checked={isDone} onChange={handleOnChange} />
+      <TodoItemName name={name} isDone={isDone} />
+      <button onClick={handleDeleteClick}>Delete</button>
+    </div>
+  );
+}
